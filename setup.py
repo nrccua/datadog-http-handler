8633ff92b1fcc0e69667feb54becb301f97c3b93
tim.reichard <tim.reichard@nrccua.org>
2019-05-30 13:39:41 -0500

    Adding setup dependencies.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md', 'r') as handle:
       long_description = convert(handle.read())
 
 setup(name='datadog-http-handler',
-      version='1.0.1',
+      version='1.0.2',
       description='Python logging module allowing you to log directly to Datadog via https',
       long_description=long_description,
       url='https://github.com/nrccua/datadog-http-handler',
@@ -17,7 +17,7 @@ setup(name='datadog-http-handler',
       license='MIT',
       packages=['datadog_http_handler'],
       install_requires=['requests'],
-      setup_requires=['pytest-runner'],
+      setup_requires=['pytest-runner', 'm2r', 'twine'],
       tests_require=['pytest'],
       zip_safe=False,
       classifiers=[
